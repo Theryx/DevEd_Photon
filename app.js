2ec70f15ddb90ff27308d09689d14c9c17880f81
Africa <ndouken@gmail.com>
2021-02-15 06:22:55 +0100

    HTML and CSS added

---
+++ app.js
@@ -0,0 +1,24 @@
+const auth = "563492ad6f91700001000001e006713990f24f83a3060b9ee15ea20e"; //ADD THE AUTH KEY
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const form = document.querySelector(".search-form");
+let searchValue;
+
+async function curatedPhotos() {
+  const dataFetch = await fetch(
+    "https://api.pexels.com/v1/search?query=nature&per_page=1",
+    {
+      method: "GET",
+      headers: { Accept: "application/json", Authorization: auth },
+    }
+  );
+  const data = await dataFetch.json();
+  data.photo.forEach((photo) => {
+    const gallerryImage = document.createElement("div");
+    gallerryImage.classList.add(gallerry - img);
+    gallerryImage.innerHTML = `<img src= ${photo.src.large}></img>
+    <p>${photo.photographer}</p>`;
+    gallery.appendChild(gallerryImage);
+  });
+}
+curatedPhotos();
