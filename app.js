1788a2e49eb500a3ab5f5f277b41ae381e27e551
Africa <ndouken@gmail.com>
2021-02-16 04:58:59 +0100

    Search funtionality enabled

--- app.js
+++ app.js
@@ -4,21 +4,55 @@ const searchInput = document.querySelector(".search-input");
 const form = document.querySelector(".search-form");
 let searchValue;
 
-async function curatedPhotos() {
-  const dataFetch = await fetch(
-    "https://api.pexels.com/v1/search?query=nature&per_page=1",
-    {
-      method: "GET",
-      headers: { Accept: "application/json", Authorization: auth },
-    }
-  );
+//Eventlsteners
+searchInput.addEventListener("input", updateInput);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchPhotos(searchValue);
+});
+
+function updateInput(e) {
+  searchValue = e.target.value;
+}
+
+async function fetchApi(url) {
+  const dataFetch = await fetch(url, {
+    method: "GET",
+    headers: { Accept: "application/json", Authorization: auth },
+  });
   const data = await dataFetch.json();
+  return data;
+}
+
+function generatePictures(data) {
   data.photos.forEach((photo) => {
     const gallerryImage = document.createElement("div");
     gallerryImage.classList.add("gallery-img");
-    gallerryImage.innerHTML = `<img src= ${photo.src.large}></img>
-    <p>${photo.photographer}</p>`;
+    gallerryImage.innerHTML = `
+    <div class = gallery-info>
+    <p>${photo.photographer}</p>
+    <a href =${photo.src.original}>Download</a>
+    </div>
+    <img src= ${photo.src.large}></img>`;
     gallery.appendChild(gallerryImage);
   });
+}
+
+async function curatedPhotos() {
+  const data = await fetchApi("https://api.pexels.com/v1/curated?per_page=1");
+  generatePictures(data);
+}
+
+async function searchPhotos(query) {
+  clear();
+  const data = await fetchApi(
+    `https://api.pexels.com/v1/search?query=${query}+query&per_page=1`
+  );
+  generatePictures(data);
+}
+
+function clear() {
+  gallery.innerHTML = "";
+  searchInput.value = "";
 }
 curatedPhotos();
